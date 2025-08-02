16c7efcd7ee67d133b076cfd12b7505832f112e1
Christopher Eby <kreed@kreed.org>
2025-08-02 02:44:27 -0700

    Add dash-to-panel; remove dash-to-dock and logo menu

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,3 +5,7 @@ set -ouex pipefail
 ### Install packages
 
 dnf5 install -y cargo neovim
+
+dnf5 install -y gnome-shell-extension-dash-to-panel
+dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
+
