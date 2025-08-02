2cfc552e49866fa814c8c2760ef4c2cc0cb34207
Christopher Eby <kreed@kreed.org>
2025-08-02 13:10:12 -0700

    Add cleanup of /var and /tmp

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,3 +9,8 @@ dnf5 install -y cargo neovim
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
 
+### Cleanup
+
+rm -rf /tmp/* || true
+find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
+find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
