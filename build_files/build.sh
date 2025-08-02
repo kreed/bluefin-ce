cb2d3c45a7eb3f82c8a4ebc04d587807d50b31cf
Christopher Eby <kreed@kreed.org>
2025-08-02 03:03:58 -0700

    Suppress countme

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,9 +3,11 @@
 set -ouex pipefail
 
 ### Install packages
+systemctl disable rpm-ostree-countme.service
 
 dnf5 install -y cargo neovim
 
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
 
+systemctl enable rpm-ostree-countme.service
