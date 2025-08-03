8234c1a428d660cf785dc82308ec9cbf4b592bda
Christopher Eby <kreed@kreed.org>
2025-08-03 14:19:12 -0700

    Kernel 6.16

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,11 +4,16 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf5 install -y cargo neovim git-delta
+dnf5 install -y neovim git-delta
 
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
 
-### Cleanup
+dnf5 -y copr enable kreed/kernel-sandbox
+dnf5 -y versionlock delete kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+dnf5 -y remove kmod-framework-laptop kmod-openrazer kmod-kvmfr kmod-v4l2loopback
+dnf5 -y update kernel\*
+dnf5 -y copr disable kreed/kernel-sandbox
 
-rm -r /var/lib/dnf
+### Cleanup
+rm -rf /var/lib/dnf
