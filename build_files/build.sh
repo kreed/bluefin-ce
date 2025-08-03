6f5e2e28739875e032d7374c0a61c4838a6201d9
Christopher Eby <kreed@kreed.org>
2025-08-03 14:32:10 -0700

    Add git-delta

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,13 +4,11 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf5 install -y cargo neovim
+dnf5 install -y cargo neovim git-delta
 
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
 
 ### Cleanup
 
-rm -rf /tmp/* || true
-find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
-find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
+rm -r /var/lib/dnf
