574d30320128787e0a11515667a1329696f1f586
Christopher Eby <kreed@kreed.org>
2025-08-05 20:26:59 -0700

    Remove cargo/rust
    I've come around to devcontainers. Rust can live there.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf5 install -y cargo neovim git-delta
+dnf5 install -y neovim git-delta
 
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
