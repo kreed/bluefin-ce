0c1ed7d62d0c6978da7ae69bfefbae3e5c930ced
Christopher Eby <kreed@kreed.org>
2025-08-05 20:23:45 -0700

    Revert "Kernel 6.16"
    This reverts commit 8234c1a428d660cf785dc82308ec9cbf4b592bda.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,16 +4,11 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf5 install -y neovim git-delta
+dnf5 install -y cargo neovim git-delta
 
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
 
-dnf5 -y copr enable kreed/kernel-sandbox
-dnf5 -y versionlock delete kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-dnf5 -y remove kmod-framework-laptop kmod-openrazer kmod-kvmfr kmod-v4l2loopback
-dnf5 -y update kernel\*
-dnf5 -y copr disable kreed/kernel-sandbox
-
 ### Cleanup
-rm -rf /var/lib/dnf
+
+rm -r /var/lib/dnf
