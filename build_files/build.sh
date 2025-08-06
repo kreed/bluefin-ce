3289ee18a0ca22c280a9371b976f4ba538ccebb0
Christopher Eby <kreed@kreed.org>
2025-08-05 20:29:01 -0700

    Remove bluefin logo from plymouth
    Love the dino. Just not at this resolution.

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,9 @@ dnf5 install -y neovim git-delta
 dnf5 install -y gnome-shell-extension-dash-to-panel
 dnf5 remove -y gnome-shell-extension-dash-to-dock gnome-shell-extension-logo-menu
 
+# hide bluefin logo in plymouth
+rm /usr/share/plymouth/themes/spinner/watermark.png
+
 ### Cleanup
 
 rm -r /var/lib/dnf
