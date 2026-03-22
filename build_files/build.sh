ede46e7357f298bdfcc221be97660312382e86a3
Christopher Eby <kreed@kreed.org>
2026-03-22 15:17:58 -0700

    build framework akmods manually

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,16 +2,56 @@
 
 set -ouex pipefail
 
-### Kernel - update to 6.19.9 from testing repo (MES hang fix)
+### Kernel - update from testing repo (MES hang fix)
+
+# Remove version lock, old kmods, and update kernel from testing repo
 dnf5 versionlock delete kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra || true
-dnf5 -y update --enablerepo=updates-testing \
-  kernel \
-  kernel-devel \
-  kernel-devel-matched \
-  kernel-core \
-  kernel-modules \
-  kernel-modules-core \
-  kernel-modules-extra
+dnf5 -y remove \
+    kmod-framework-laptop framework-laptop-kmod-common \
+    kmod-v4l2loopback v4l2loopback || true
+dnf5 -y --enablerepo=updates-testing --setopt=tsflags=noscripts update \
+    kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel kernel-devel-matched
+dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
+KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+ARCH="$(uname -m)"
+RELEASE="$(rpm -E '%fedora')"
+
+# Build kmods against new kernel
+chmod 1777 /tmp
+dnf5 -y install akmods
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+
+# Install akmod source packages
+dnf5 -y install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"${RELEASE}".noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"${RELEASE}".noarch.rpm
+dnf5 -y --setopt=tsflags=noscripts install \
+    akmod-framework-laptop-*.fc"${RELEASE}"."${ARCH}" \
+    akmod-v4l2loopback-*.fc"${RELEASE}"."${ARCH}"
+
+# Build and verify kmods
+for kmod in framework-laptop v4l2loopback; do
+    akmods --force --kernels "${KERNEL}" --kmod "${kmod}"
+    find /var/cache/akmods/"${kmod}"/ -name \*.log -print -exec cat {} \;
+done
+modinfo /usr/lib/modules/"${KERNEL}"/extra/framework-laptop/framework_laptop.ko.xz > /dev/null
+modinfo /usr/lib/modules/"${KERNEL}"/extra/v4l2loopback/v4l2loopback.ko.xz > /dev/null
+
+# Install built kmods
+dnf5 -y install \
+    /var/cache/akmods/framework-laptop/kmod-framework-laptop-*.rpm \
+    /var/cache/akmods/v4l2loopback/kmod-v4l2loopback-*.rpm \
+    framework-laptop-kmod-common \
+    v4l2loopback
+# Clean up build dependencies
+dnf5 -y remove \
+    rpmfusion-free-release rpmfusion-nonfree-release \
+    akmod-framework-laptop akmod-v4l2loopback \
+    akmods \
+    kernel-devel kernel-devel-matched
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+rm -rf /var/cache/akmods
 
 ### Packages
 
